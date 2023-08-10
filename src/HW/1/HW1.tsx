2fc6425b8d9fa50ea4f0b2fc6b1e6fdcc6c563f6
ALbirdEX <Alex123890@gmail.com>
2023-08-10 22:55:15 +0300

    fixed the app

--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -23,8 +23,7 @@ export const HW1 = () => {
 		{id: 6, name: 'James', age: 18, address: {street: '987 Pine Rd', city: 'Dallas'}},
 		{id: 7, name: 'Jennifer', age: 21, address: {street: '654 Birch Ct', city: 'Seattle'}},
 		{id: 8, name: 'Robert', age: 4, address: {street: '876 Spruce Way', city: 'San Francisco'}},
-		{
-			id: 9,
+		{id: 9,
 			name: 'Jessica',
 			age: 12,
 			address: {street: '543 Willow Dr', city: 'San Francisco'},
@@ -34,7 +33,7 @@ export const HW1 = () => {
 
 	return (
 		<div id={'hw01'}>
-			{/*<UserList users={'НАЗВАНИЕ МАССИВА, КОТОРЫЙ МЫ ПЕРЕДАЕМ'} />*/}
+			<UserList users={users} />
 		</div>
 	);
 };
